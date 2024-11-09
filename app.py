c5eea915ccd573290cdde51c35614887de7dd4ac
CamiAni <cwilverht@fi.uba.ar>
2024-11-09 17:58:35 -0300

    Inicio terminado, al menos un 90 porciento terminado

--- app.py
+++ app.py
@@ -4,7 +4,19 @@ app=Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    #Esto creo que usando apis se puede evitar hardcodear
+    datosIntegrantes = {
+        "1": {"nombre":"Camila Pratto", "descripcion":"Ingenieria en Informatica", "imagenUrl":"imagenes/persona1.png"},
+        "2": {"nombre":"Camila Anahi Wilverht Rohr", "descripcion":"Ingenieria en Informatica", "imagenUrl":"imagenes/persona2.png"},
+        "3": {"nombre": "Francisca Gaillard", "descripcion" : "Ingenieria en Informatica", "imagenUrl":"imagenes/persona3.png"},
+        "4": {"nombre": "Ignacio Cettour", "descripcion" : "Ingenieria en Informatica", "imagenUrl":"imagenes/persona4.png"},
+        "5": {"nombre": "Lara Ovejero", "descripcion" : "Ingenieria en Informatica", "imagenUrl":"imagenes/persona5.png"},
+        "6": {"nombre": "Matias Rigano", "descripcion" : "Ingenieria en Informatica", "imagenUrl":"imagenes/persona6.png"},
+        "7": {"nombre": "Victor Oliva", "descripcion" : "Ingenieria en Informatica", "imagenUrl":"imagenes/persona7.png"},
+        "8": {"nombre": "Leonel Chaves", "descripcion" : "Profesor en la UBA y corrector del proyecto.", "imagenUrl":"imagenes/persona8.png"}
+
+    }
+    return render_template("index.html", datosIntegrantes=datosIntegrantes)
 
 @app.route('/')
 def base():
@@ -30,6 +42,21 @@ def publicarMascotas():
 def registrarse():
     return render_template("registrarse.html")
 
+@app.route('/integrantes')
+def integrantes():
+    #Esto creo que usando apis se puede evitar hardcodear
+    datosIntegrantes = {
+        "1": {"nombre":"Camila Pratto", "descripcion":"Estudiante de la Facultad de Ingenieria de la UBA. Estudia la carrera de Informatica.", "imagenUrl":"imagenes/persona1.png"},
+        "2": {"nombre":"Camila Anahi Wilverht Rohr", "descripcion":"Estudiante de la Facultad de Ingenieria de la UBA. Estudia la carrera de Informatica.", "imagenUrl":"imagenes/persona2.png"},
+        "3": {"nombre": "Francisca Gaillard", "descripcion" : "Estudiante de la Facultad de Ingenieria de la UBA. Estudia la carrera de Informatica.", "imagenUrl":"imagenes/persona3.png"},
+        "4": {"nombre": "Ignacio Cettour", "descripcion" : "Estudiante de la Facultad de Ingenieria de la UBA. Estudia la carrera de Informatica.", "imagenUrl":"imagenes/persona4.png"},
+        "5": {"nombre": "Lara Ovejero", "descripcion" : "Estudiante de la Facultad de Ingenieria de la UBA. Estudia la carrera de Informatica.", "imagenUrl":"imagenes/persona5.png"},
+        "6": {"nombre": "Matias Rigano", "descripcion" : "Estudiante de la Facultad de Ingenieria de la UBA. Estudia la carrera de Informatica.", "imagenUrl":"imagenes/persona6.png"},
+        "7": {"nombre": "Victor Oliva", "descripcion" : "Estudiante de la Facultad de Ingenieria de la UBA. Estudia la carrera de Informatica.", "imagenUrl":"imagenes/persona7.png"},
+        "8": {"nombre": "Leonel Chaves", "descripcion" : "Profesor en la UBA y corrector del proyecto.", "imagenUrl":"imagenes/persona8.png"}
+
+    }
+    return render_template("integrantes.html", datosIntegrantes= datosIntegrantes)
 
 
 if __name__ == "__main__":
