c4eba4b8c76d918e22fbd1287d6e8ec2901c9d64
cpratto <camilapratto0@gmail.com>
2024-11-11 10:22:15 -0300

    info diccionario

--- app.py
+++ app.py
@@ -8,49 +8,56 @@ mascotas = {
         'descripcion': 'Mascota simpática y amigable',
         'imagenUrl': 'imagenes/Cheems.jpg',
         'edad': '3 años',
-        'raza': 'Shiba Inu'
+        'raza': 'Shiba Inu',
+        'zona': 'Belgrano'
     },
     2: {
         'nombre': 'Bobby',
         'descripcion': 'Perro enérgico y leal',
         'imagenUrl': 'imagenes/Bobby.jpg',
         'edad': '5 años',
-        'raza': 'Golden Retriever'
+        'raza': 'Golden Retriever',
+        'zona': 'Villa crespo'
     },
     3: {
         'nombre': 'Luna',
         'descripcion': 'Gatita juguetona y cariñosa',
         'imagenUrl': 'imagenes/Luna.jpg',
         'edad': '2 años',
-        'raza': 'Siamesa'
+        'raza': 'Siamesa',
+        'zona': 'Villa Urquiza'
     },
     4: {
         'nombre': 'copito',
         'descripcion': 'conejo amigable y dienton',
         'imagenUrl': 'imagenes/copito.jpg',
         'edad': '5 meses',
-        'raza': 'conejo'
+        'raza': 'conejo',
+        'zona': 'Once'
     },
     5: {
         'nombre': 'franklin',
         'descripcion': 'tortuga timida e incomprendida',
         'imagenUrl': 'imagenes/copitofranklin.jpg',
         'edad': '15 años',
-        'raza': 'tortuga'
+        'raza': 'tortuga',
+        'zona': 'Tigre'
     },  
     6: {
         'nombre': 'Tomi',
         'descripcion': 'Perro jugueton',
         'imagenUrl': 'imagenes/Tomi.jpg',
         'edad': '6 años',
-        'raza': 'Border Collie'
+        'raza': 'Border Collie',
+        'zona': 'Palermo'
     },  
     7: {
         'nombre': 'Mara',
         'descripcion': 'Perra tranquila',
         'imagenUrl': 'imagenes/Mara.jpg',
         'edad': '4 años',
-        'raza': 'Pug'
+        'raza': 'Pug',
+        'zona': 'Recoleta'
     },  
 }
 
