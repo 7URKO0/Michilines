b21659baa6bc89817330dc07cc8b42a84351eb18
CamiAni <cwilverht@fi.uba.ar>
2024-11-12 22:14:47 -0300

    Actualizo el app.py, el PerfilMascota.css, las imagenes, galeria.html y perfilMascota.html que estaban vacios, no se por que...

---
+++ static/js/galeria.js
@@ -0,0 +1,13 @@
+function filterGalleryBySelect() {
+    const category = document.getElementById('categorySelect').value;
+    const items = document.querySelectorAll('.gallery-item');
+  
+    items.forEach(item => {
+      if (category === 'all' || item.classList.contains(category)) {
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+  }
+  
