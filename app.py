8956be9a2c18fb2fb833854d899f6e36379fbd60
VictorAOM <voliva@fi.uba.ar>
2024-11-16 20:21:48 -0300

    .

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:tu_contraseña@localhost/michilines'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:michilines_usuario@localhost/michilines'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
